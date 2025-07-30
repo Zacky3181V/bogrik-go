3cd04b72a2ea1fe70435f0d23692133086d9f4f4
Daniyar Alkulov <medvedicabolsaa@gmail.com>
2025-07-30 16:45:53 +0200

    added small templating to the project. Less code repition
    work is still in progress.

--- main.go
+++ main.go
@@ -16,29 +16,61 @@ func main() {
 	})
 
 	r.GET("/portfolio", func(c *gin.Context) {
-		c.HTML(200, "portfolio.html", gin.H{})
+		c.HTML(200, "portfolio.html", gin.H{
+			"Title": "Portfolio",
+			"MainTitle": "Bogrik",
+			"SubTitle": "Portfolio",
+		})
 	})
 
 	r.GET("/digital-painting", func(c *gin.Context) {
-		c.HTML(200, "digital-painting.html", gin.H{})
+		c.HTML(200, "digital-painting.html", gin.H{
+			"Title": "Digital Painting",
+			"MainTitle": "Digital Painting",
+			"SubTitle": "Bogrik",
+		})
 	})
 	r.GET("/malutka-pig", func(c *gin.Context) {
-		c.HTML(200, "malutka-pig.html", gin.H{})
+		c.HTML(200, "malutka-pig.html", gin.H{
+			"Title": "Malutka Pig",
+			"MainTitle": "Malutka Pig",
+			"SubTitle": "Bogrik",
+		})
 	})
 	r.GET("/weird", func(c *gin.Context) {
-		c.HTML(200, "weird.html", gin.H{})
+		c.HTML(200, "weird.html", gin.H{
+			"Title": "Weird",
+			"MainTitle": "Weird",
+			"SubTitle": "Bogrik",
+		})
 	})
 	r.GET("/random", func(c *gin.Context) {
-		c.HTML(200, "random.html", gin.H{})
+		c.HTML(200, "random.html", gin.H{
+			"Title": "Random",
+			"MainTitle": "Random",
+			"SubTitle": "Bogrik",
+		})
 	})
 	r.GET("/moral", func(c *gin.Context) {
-		c.HTML(200, "moral.html", gin.H{})
+		c.HTML(200, "moral.html", gin.H{
+			"Title": "Moral",
+			"MainTitle": "Moral",
+			"SubTitle": "Bogrik",
+		})
 	})
 	r.GET("/marvin", func(c *gin.Context) {
-		c.HTML(200, "marvin.html", gin.H{})
+		c.HTML(200, "marvin.html", gin.H{
+			"Title": "Marvin",
+			"MainTitle": "Marvin",
+			"SubTitle": "Bogrik",
+		})
 	})
 	r.GET("/unibo", func(c *gin.Context) {
-		c.HTML(200, "unibo.html", gin.H{})
+		c.HTML(200, "unibo.html", gin.H{
+			"Title": "UniBo",
+			"MainTitle": "UniBo",
+			"SubTitle": "Bogrik",
+		})
 	})
 
 	r.Run(":8085")
